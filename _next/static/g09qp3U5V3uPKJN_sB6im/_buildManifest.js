64eef4e2035c4a3bab5d86a3e6683a0dcc920b53
nuharaomesh <omeshnuhara4@gmail.com>
2026-03-04 13:14:39 +0530

    Updates

--- _next/static/g09qp3U5V3uPKJN_sB6im/_buildManifest.js
+++ _next/static/g09qp3U5V3uPKJN_sB6im/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nutyre-landing//_next/:path+",
+        "destination": "/nutyre-landing/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
